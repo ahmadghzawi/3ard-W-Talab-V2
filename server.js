80392131c711bf68ec8c73f2eecab929f106b12e
ahmadghzawi <ahmd.ghzawi@gmail.com>
2019-12-28 02:33:48 +0200

    modify port

--- server.js
+++ server.js
@@ -13,5 +13,15 @@ app.use(process.env.USER_ROUTE_URL, userRouter)     //      /users/API/
 const postRouter = require('./routes/posts')
 app.use(process.env.POST_ROUTE_URL, postRouter)     //      /posts/API/
 
-app.listen(process.env.PORT || 9000)
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("dashboard/build"));
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "dashboard", "build", "index.html"));
+    });
+  }
+  
+  const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`server started on port ${PORT}`))
 
