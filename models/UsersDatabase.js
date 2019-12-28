d21f92063efc303210f7af04242895b6298c2fae
ahmadghzawi <ahmd.ghzawi@gmail.com>
2019-12-28 18:54:34 +0200

    fix database connection

--- models/UsersDatabase.js
+++ models/UsersDatabase.js
@@ -1,4 +1,12 @@
-require('./database.js')
+require("dotenv").config();
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+const db = mongoose.connection;
+db.on("error", err => console.err(err));
+db.once("open", () => console.log(`Connected to users database`));
 
 const usersSchema = new mongoose.Schema({
   username: String,
--- models/productsDatabase.js
+++ models/productsDatabase.js
@@ -1,4 +1,12 @@
-require('./database.js')
+require("dotenv").config();
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+const db = mongoose.connection;
+db.on("error", error => console.err(error));
+db.once("open", () => console.log(`Connected to Posts database`));
 
 const productsSchema = new mongoose.Schema(
   {
