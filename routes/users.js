a5fa518120797afb14d98d5a724da1374070f382
ahmadghzawi <ahmd.ghzawi@gmail.com>
2019-12-29 23:29:27 +0200

    fix edit profile modal

--- routes/users.js
+++ routes/users.js
@@ -21,7 +21,7 @@ router.put("/editProfile", (req, res) => {
       if (error) {
         res.status(500).json({ message: error.message });
       } else {
-        res.status(200).json(req.body);
+        res.status(200).json(req.params);
       }
     }
   );
